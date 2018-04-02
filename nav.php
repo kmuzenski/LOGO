333f2d14143df0888a96620aed80fdb28dfa24bb
[kmuzenski] <kathrynmuzenski@yahoo.com>
2018-04-02 18:47:30 -0500

    testing nav

--- nav.php
+++ nav.php
@@ -6,8 +6,8 @@
 		<button type="button" class="navbar-toggle btn-custom" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
          	<span class="sr-only">Toggle navigation</span>
          	<span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
         </button>
          
          <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" width="50"></a>		
@@ -19,14 +19,25 @@
           	
 		<ul class="nav navbar-nav">
 		
-		<li><a href="index.php"><p>Home</p></a></li>
-    <li><a href="contact.php"><p>Contact</p></a></li>
+		<li class="active"><a href="#"><p>Home</p></a></li>
+
+    <li class="dropdown">
+      <a class="dropdown-toggle" data-toggle="dropdown" href="contact.php">
+        <p>Contact<span class="caret"></span></p>
+    </a>
+        <ul class="dropdown-menu">
+          <li><a href="faq.php">FAQ</a></li>
+          <li><a href="office.php">Offices</a></li>
+        </ul>
     <li><a href="gallery.php"><p>Gallery</p></a></li>
     <li><a href="properties.php"><p>Properties</p></a></li>
 
    </ul>
 
-            					
+   <ul class="nav navbar-nav navbar-right">
+    <li><a href="#">Login</a></li>
+
+    </ul>
        </div>
    </div>
 
