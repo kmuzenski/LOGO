201ee805e59680de0ade9455ce1e177f87fe2de3
[kmuzenski] <kathrynmuzenski@yahoo.com>
2018-04-02 21:15:47 -0500

    style fix

--- gallery.php
+++ gallery.php
@@ -57,10 +57,10 @@
   <div class="numbertext"><center>4 / 4</center></div>
 </div>
 
-
+<center>
 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
 <a class="next" onclick="plusSlides(1)">&#10095;</a>
-
+</center>
 </div>
 
 
