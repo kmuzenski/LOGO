1a1f63a85b7f12a59a8d3ee8255ff6cb386b36a9
[kmuzenski] <kathrynmuzenski@yahoo.com>
2018-04-02 20:25:14 -0500

    + faq and contact pages

---
+++ contacts.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<?php require_once('header.php'); ?>
+
+<body>
+
+
+
+<?php require_once('nav.php'); ?>
+<br><br><br><br><br><br>
+
+<div class="container">
+	<div class="row divBorder">
+		<div class="col-xs-12 col-md-12 col-lg-12">
+			<h1>Phone &amp; Email</h1>
+			<h3>555-555-555<br> email000@here.com</h3>
+		</div>
+	</div>
+	<div class="row divBorder">
+		<div class="col-xs-12 col-md-12 col-lg-12">
+			<h1>Social Media</h1>
+			<h3>Instagram <br> Facebook </h3>
+		</div>
+	</div>
+
+</div> <!-- container -- >
+
+<br><br><br><br><br>
+<?php require_once('footer.php'); ?>
+
+</body>
+</html>
---
+++ faq.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<?php require_once('header.php'); ?>
+
+<body>
+
+
+
+<?php require_once('nav.php'); ?>
+<br><br><br><br><br><br>
+
+<h1>Frequently Asked Questions</h1>
+<br>
+<h3>Q: Questions are shown here</h3>
+<p>A: Answer is shown here</p>
+<br><br>
+<h3>Q: Questions are shown here</h3>
+<p>A: Answer is shown here</p>
+<br><br>
+<h3>Q: Questions are shown here</h3>
+<p>A: Answer is shown here</p>
+
+<br><br><br><br><br>
+<?php require_once('footer.php'); ?>
+
+</body>
+</html>
